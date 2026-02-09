77495c1b20b44c2cdec046ec3b30ffbfe6ced8b2
Eetu Oinonen <eetu.oinonen04@gmail.com>
2026-02-09 14:16:54 +0200

    refactored noteform into its own component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,10 @@
-import { Container, Row, Col, Button, Form, Card } from "react-bootstrap"
 import { useState, useEffect } from "react"
-import { addNoteToUserWithId, getUserNotesById} from "../services/notes.js"
-import RenderRecentNotes from "../components/RenderRecentNotes.jsx"
+import { getUserNotesById} from "../services/notes.js"
+import NoteForm from "../components/NoteForm.jsx"
 
 const Dashboard = ({ user, setMessage }) => {
-    const [title, setTitle] = useState("")
-    const [noteContent, setNoteContent] = useState("")
-    const [isFormVisible, setIsFormVisible] = useState(false)
     const [userNotes, setUserNotes] = useState([])
-    
+
     useEffect(() => {
         const fetchNotes = async () => {
             const notes = await getUserNotesById(user.id)
@@ -17,62 +13,12 @@ const Dashboard = ({ user, setMessage }) => {
         if (user.id) fetchNotes()
     }, [])
 
-    const handleSubmit = async event => {
-        event.preventDefault()
-
-        if (!title || !noteContent) return
-
-        //folderId on nyt null, mutta tulee tarpeen myöhemmin
-        const dataObject = {
-            "title": title,
-            "content": noteContent,
-            "id": user.id,
-            "folderId": null
-        }
-
-        try {
-            const createdNote = await addNoteToUserWithId(dataObject)
-
-            setTitle("")
-            setNoteContent("")
-            setIsFormVisible(false)
-            setMessage(`Succesfully created note: ${createdNote.title}`)
-            setTimeout(() => {setMessage(null)}, 4000)
-        } catch {
-            console.log("Tapahtui virhe lisättäessä muistiinpanoa")
-        }
-    }
-
     return (
-        <Container fluid className="vh-100 p-0">
-            <Row className="g-0 h-100">
-                <Col xs={3} md={2} className="bg-dark text-white p-3">
-                    <h4>KBase</h4>
-                    <hr/>
-                    <RenderRecentNotes fiveMostRecent={userNotes.slice(0, 5)}/>
-                </Col>
-
-                <Col xs={9} md={10} className="bg-light p-4">
-                    <h2>Main Dashboard</h2>
-                    <Button onClick={() => setIsFormVisible(true)} style={{display: isFormVisible ? "none" : "block"}}>Create a new note</Button>
-
-                    <Form style={{display: isFormVisible ? "block" : "none"}} onSubmit={handleSubmit}>
-                        <Card style={{width: "400px"}} className="p-4 shadow">
-                            <Form.Group className="mb-3">
-                                <Form.Label>Title</Form.Label>
-                                <Form.Control value={title} onChange={(event) => setTitle(event.target.value)}></Form.Control>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3">
-                                <Form.Control value={noteContent} onChange={(event) => setNoteContent(event.target.value)} placeholder="Your note starts here..." type="text" as="textarea"></Form.Control>
-                            </Form.Group>
-
-                            <Button variant="primary" type="submit" className="w-100">Create note</Button>
-                        </Card>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+        <NoteForm 
+            user={user}
+            userNotes={userNotes}
+            setMessage={setMessage}
+        />
     )
 }
 
